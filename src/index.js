70f0bd9db0f7571a9160614921fff62eb17a01fc
sandeepk275 <sandeep.kumar41991@gmail.com>
2022-02-21 22:54:34 +0530

    final submit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app';
-//import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
